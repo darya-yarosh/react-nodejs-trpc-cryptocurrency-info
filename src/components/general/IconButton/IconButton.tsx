c802185b83c82b50b10dd3813b2d6ccb381c0585
darya-yarosh <gurimusagi@yandex.ru>
2023-10-11 17:24:32 +0300

    Created button with icon

---
+++ src/components/general/IconButton/IconButton.tsx
@@ -0,0 +1,31 @@
+import styles from "components/general/IconButton/IconButton.module.scss";
+
+interface IconButtonProps {
+    iconSVG: string;
+    caption: string;
+    onClick: () => void;
+    sizePX?: number;
+}
+
+export default function IconButton({
+    iconSVG,
+    caption,
+    onClick,
+    sizePX = 28
+}: IconButtonProps) {
+    const wrapperSize = `${sizePX}px`;
+    const iconSize = `${sizePX - 8}px`;
+
+    return <button className={styles.wrapper}
+        onClick={onClick}
+        type="button"
+        style={{ width: wrapperSize, height: wrapperSize }}
+    >
+        <img
+            className={styles.icon}
+            src={iconSVG}
+            alt={caption}
+            style={{ width: iconSize, height: iconSize }}
+        />
+    </button>
+}

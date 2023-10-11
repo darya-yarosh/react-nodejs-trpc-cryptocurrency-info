86462f1b24e1734684d883ca24b29f3de441643e
darya-yarosh <gurimusagi@yandex.ru>
2023-10-11 17:26:04 +0300

    Created page for coin list

---
+++ src/components/pages/CoinListPage/CoinListPage.tsx
@@ -0,0 +1,31 @@
+import { useMemo, useState } from "react";
+
+import Coin from "models/Coin";
+import { SEARCH_PLACEHOLDER } from "models/Interface";
+
+import CoinTable from "components/CoinTable/CoinTable";
+import SearchInput from "components/general/SearchInput/SearchInput";
+
+import { getFilteredCoinList } from "logic/utils/Helper";
+
+import styles from "components/pages/CoinListPage/CoinListPage.module.scss";
+
+interface CoinListPageProps {
+    coinList: Coin[];
+}
+
+export default function CoinListPage({
+    coinList
+}: CoinListPageProps) {
+    const [searchFilter, setSearchFilter] = useState<string>("");
+    const filteredCoinList = useMemo(() => getFilteredCoinList(coinList, searchFilter), [searchFilter, coinList]);
+
+    return <div className={styles.wrapper}>
+        <header className={styles.header}>
+            <SearchInput value={searchFilter} placeholderValue={SEARCH_PLACEHOLDER} onChange={setSearchFilter} />
+        </header>
+        <section>
+            <CoinTable coinList={filteredCoinList} />
+        </section>
+    </div>
+}

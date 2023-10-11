4aa4c5c2fedf9aee76f88b855c1454d87dad1ce3
darya-yarosh <gurimusagi@yandex.ru>
2023-10-11 17:22:29 +0300

    Created icon-element for sorting status

---
+++ src/components/SortIcon/SortIcon.tsx
@@ -0,0 +1,27 @@
+import { SortOrder } from "models/Interface";
+
+import styles from "components/SortIcon/Sorticon.module.scss";
+
+interface SortIconProps {
+    sortType: string;
+    sortOrder: SortOrder;
+    isEnabled: boolean;
+    isSelected: boolean;
+}
+
+export default function SortIcon({
+    sortType,
+    sortOrder,
+    isEnabled,
+    isSelected
+}: SortIconProps) {
+    const src = `
+        images/sort/sort-${isEnabled && isSelected
+            ? sortOrder.toLowerCase()
+            : isEnabled
+                ? "enabled"
+                : "disabled"}.svg`;
+    const alt = `sort icon for ${sortType}`;
+
+    return <img className={styles.icon} src={`${src}`} alt={alt} />
+}

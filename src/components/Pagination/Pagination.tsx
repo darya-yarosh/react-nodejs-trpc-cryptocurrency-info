1d100d93eb93c9429848c25a6614f971179bb95f
darya-yarosh <gurimusagi@yandex.ru>
2023-10-12 15:06:32 +0300

    Created pagination element

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,18 @@
+import styles from "components/Pagination/Pagination.module.scss";
+
+interface PaginationProps {
+    pages: number[]
+    currentPage: number;
+    changePage: (pageIndex: number) => void
+}
+
+export default function Pagination({
+    pages,
+    currentPage,
+    changePage
+}: PaginationProps) {
+    return <nav className={styles.pagination}>{pages.map(pageNum => {
+        const className = pageNum === currentPage ? styles.pageNum__current : styles.pageNum;
+        return <button key={pageNum} className={className} onClick={() => changePage(pageNum)}>{pageNum + 1}</button>
+    })}</nav>
+}

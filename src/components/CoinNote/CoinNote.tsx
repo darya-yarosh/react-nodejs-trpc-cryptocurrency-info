4db89f333dbb0da8d6b318c01b29f9c61395355c
darya-yarosh <gurimusagi@yandex.ru>
2023-10-12 14:55:16 +0300

    Created component CoinNote

---
+++ src/components/CoinNote/CoinNote.tsx
@@ -0,0 +1,24 @@
+import Coin from "models/Coin";
+
+import IconButton from "components/general/IconButton/IconButton";
+
+import styles from "components/CoinNote/CoinNote.module.scss";
+
+interface CoinNoteProps {
+    coin: Coin;
+}
+
+export default function CoinNote({
+    coin
+}: CoinNoteProps) {
+    return <tr key={coin.id} className={styles.note__wrapper}>
+        <td><p>{coin.rank}</p></td>
+        <td><p>{coin.symbol}</p></td>
+        <td><img src={`${coin.logo}`} alt={`${coin.id} icon`} width={"25px"}></img></td>
+        <td><p>{coin.name}</p></td>
+        <td><p>{coin.priceUsd}</p></td>
+        <td><p>{coin.marketCapUsd}</p></td>
+        <td><p>{coin.changePercent24Hr}</p></td>
+        <td><IconButton caption={`Button to adding ${coin.name} in portfolio`} iconSVG={"/images/buttons/plus.svg"} onClick={() => { }} /></td>
+    </tr>
+}

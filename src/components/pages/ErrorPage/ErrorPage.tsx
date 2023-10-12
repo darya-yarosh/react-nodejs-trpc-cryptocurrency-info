fa082f48d3cca46456889c0a64b2218d0888007f
darya-yarosh <gurimusagi@yandex.ru>
2023-10-13 00:42:20 +0300

    Created Error page

---
+++ src/components/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,22 @@
+import { useRouteError } from "react-router-dom";
+
+import styles from "components/pages/ErrorPage/ErrorPage.module.scss";
+
+interface ErrorPageProps {
+    description: string
+}
+
+export default function ErrorPage({
+    description
+}: ErrorPageProps) {
+    const error = useRouteError();
+    console.log(error);
+
+    return <div className={styles.wrapper}>
+        <h1>Something wrong.</h1>
+        <p>Unexpected error has occured.</p>
+        <p>
+            {description}
+        </p>
+    </div>
+}

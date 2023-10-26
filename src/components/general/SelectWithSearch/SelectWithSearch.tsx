40048fad611ecbd0e452304ff2775afa5e26ffc9
darya-yarosh <gurimusagi@yandex.ru>
2023-10-26 18:51:35 +0300

    Created component of select with search. Created debounce hook.

---
+++ src/components/general/SelectWithSearch/SelectWithSearch.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+
+import { useDebounce } from "logic/utils/useDebounce";
+
+import styles from "components/general/SelectWithSearch/SelectWithSearch.module.scss";
+
+interface SelectWithSearchProps {
+    value: string;
+    placeholderValue: string;
+    onSearchChange: (value: string) => void;
+    onOptionSelect: (value: string) => void;
+    list: string[];
+}
+
+export default function SelectWithSearch({
+    value,
+    placeholderValue,
+    list,
+    onOptionSelect,
+    onSearchChange,
+}: SelectWithSearchProps) {
+    const [searchValue, setSearchValue] = useState<string>(value);
+    const [isFocusInput, setIsFocusInput] = useState<boolean>(false);
+
+    const reloadList = useDebounce(() => onSearchChange(searchValue), 300);
+
+    const SEARCH_ID = "searchId";
+
+    const selectClassName = isFocusInput
+        ? styles.optionList__visible
+        : styles.optionList__hidden;
+
+    function changeSearchValue(value: string) {
+        setSearchValue(value);
+    }
+
+    function setOption(value: string) {
+        onOptionSelect(value);
+        setSearchValue(value);
+        setIsFocusInput(false);
+    }
+
+    function handleInputFocus(event: React.FocusEvent<HTMLInputElement, Element>) {
+        if (event.currentTarget.id === SEARCH_ID) {
+            setIsFocusInput(true);
+        }
+    }
+
+    useEffect(() => {
+        reloadList();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [searchValue])
+
+    useEffect(() => {
+        setSearchValue(value);
+    }, [value])
+
+    return <div className={styles.wrapper}>
+        <input className={styles.input}
+            id={SEARCH_ID}
+            value={searchValue}
+            placeholder={placeholderValue}
+            onChange={(event) =>
+                changeSearchValue(event.target.value)}
+            onFocus={(event) => (handleInputFocus(event))}
+        />
+        <div className={selectClassName}>
+            {list.map((option) =>
+                <button
+                    key={option}
+                    type="button"
+                    className={styles.option}
+                    onClick={() =>
+                        setOption(option)
+                    }
+                >
+                    {option}
+                </button>
+            )}
+        </div>
+    </div>
+}
---
+++ src/logic/utils/useDebounce.ts
@@ -0,0 +1,27 @@
+import { useRef, useEffect } from "react";
+
+type Timer = ReturnType<typeof setTimeout>;
+
+export function useDebounce<Function extends (...args: any[]) => void>(
+  func: Function,
+  delay = 1000,
+) {
+  const timer = useRef<Timer>();
+
+  useEffect(() => {
+    return () => {
+      if (!timer.current) return;
+
+      clearTimeout(timer.current);
+    };
+  }, []);
+
+  const debouncedFunction = ((...args) => {
+    const newTimer = setTimeout(() => func(...args), delay);
+
+    clearTimeout(timer.current);
+    timer.current = newTimer;
+  }) as Function;
+
+  return debouncedFunction;
+}

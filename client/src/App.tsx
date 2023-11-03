1d56422c1ffccfd268918eb54b9b026704e6e1f2
darya-yarosh <gurimusagi@yandex.ru>
2023-11-03 15:49:38 +0300

    Configured the final version of the client's access to the server.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,56 @@ import { useState } from "react";
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 
 import { AppRouter } from "../../server/src/appRouter";
 
+import CoinPage from "pages/CoinPage/CoinPage";
+import ErrorPage from "pages/ErrorPage/ErrorPage";
+import CoinListPage from "pages/CoinListPage/CoinListPage";
+import PortfolioPage from "pages/PortfolioPage/PortfolioPage";
+import TransactionPage from "pages/TransactionPage/TransactionPage";
+
+import CoinsProvider from "providers/coins";
+import PortfolioProvider from "providers/portfolio";
+
+import "App.scss";
+
 const trpc = createTRPCReact<AppRouter>();
+
+export const router = createBrowserRouter(
+    createRoutesFromElements([
+        <Route
+            path="/"
+            element={<CoinListPage />}
+            errorElement={
+                <ErrorPage description="An error occurred on the main page." />
+            }
+        >
+            <Route
+                path="/portfolio"
+                element={<PortfolioPage />}
+                errorElement={
+                    <ErrorPage description="An error occurred on the portfolio page." />
+                }
+            />
+        </Route>,
+        <Route
+            path="cryptocoins/:id"
+            element={<CoinPage />}
+            errorElement={
+                <ErrorPage description="An error occurred on the coin page." />
+            }
+        />,
+        <Route
+            path="/purchase/:id?"
+            element={<TransactionPage />}
+            errorElement={
+                <ErrorPage description="An error occurred on the transaction page." />
+            }
+        />,
+    ]),
+);
 
 export default function App() {
     const [queryClient] = useState(() => new QueryClient());
@@ -20,29 +66,22 @@ export default function App() {
     );
 
     return (
-        <trpc.Provider client={trpcClient} queryClient={queryClient}>
-            <QueryClientProvider client={queryClient}>
-                <TestPage />
-            </QueryClientProvider>
-        </trpc.Provider>
-    );
-}
+        <div className="app">
+            <div className="app__wrapper">
+                <trpc.Provider client={trpcClient} queryClient={queryClient}>
+                    <QueryClientProvider client={queryClient}>
 
-export function TestPage() {
-    const coinList = trpc.getCoinList.useQuery({
-        search: null,
-        ids: null,
-        offset: 0,
-        limit: 10,
-    });
+                        <PortfolioProvider>
+                            <CoinsProvider>
 
-    console.log("Getting coins from server:", coinList.data);
+                                <RouterProvider router={router} />
 
-    if (!coinList.data) return <div>Loading...</div>;
-    return (
-        <div style={{display: 'none'}}>
-            {coinList.data.map(coin =>
-                <p key={coin.id}>{coin.id}</p>)}
+                            </CoinsProvider>
+                        </PortfolioProvider>
+
+                    </QueryClientProvider>
+                </trpc.Provider>
+            </div>
         </div>
     );
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,77 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
 import reportWebVitals from "reportWebVitals";
-
-import CoinsProvider from "providers/coins";
-import PortfolioProvider from "providers/portfolio";
-
-import CoinListPage from "pages/CoinListPage/CoinListPage";
-import CoinPage from "pages/CoinPage/CoinPage";
-import ErrorPage from "pages/ErrorPage/ErrorPage";
-import PortfolioPage from "pages/PortfolioPage/PortfolioPage";
-import TransactionPage from "pages/TransactionPage/TransactionPage";
 
 import App from "App";
 
-import "App.scss";
 import "reset.scss";
 import "index.scss";
-
-export const router = createBrowserRouter(
-  createRoutesFromElements([
-    <Route
-      path="/"
-      element={<CoinListPage />}
-      errorElement={
-        <ErrorPage description="An error occurred on the main page." />
-      }
-    >
-      <Route
-        path="/portfolio"
-        element={<PortfolioPage />}
-        errorElement={
-          <ErrorPage description="An error occurred on the portfolio page." />
-        }
-      />
-    </Route>,
-    <Route
-      path="cryptocoins/:id"
-      element={<CoinPage />}
-      errorElement={
-        <ErrorPage description="An error occurred on the coin page." />
-      }
-    />,
-    <Route
-      path="/purchase/:id?"
-      element={<TransactionPage />}
-      errorElement={
-        <ErrorPage description="An error occurred on the transaction page." />
-      }
-    />,
-  ]),
-);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <div className="app">
-      <div className="app__wrapper">
-        <PortfolioProvider>
-          <CoinsProvider>
-            <RouterProvider router={router} />
-            <App />
-          </CoinsProvider>
-        </PortfolioProvider>
-      </div>
-    </div>
+    <App />
   </React.StrictMode>,
 );
 

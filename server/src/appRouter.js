e227385996622374e458a619ca22b3cd5e602770
darya-yarosh <gurimusagi@yandex.ru>
2023-11-03 20:54:28 +0300

    Created methods for accessing CoinCap.

--- server/src/appRouter.js
+++ server/src/appRouter.js
@@ -68,24 +68,25 @@ var appRouter = trpc.router({
         throw new Error('Error[getCoinList]: Input is not a valid object.');
     })
         .query(function (opts) { return __awaiter(void 0, void 0, void 0, function () {
-        var input, requestOptions, url, apiUrl, dataInfo, coinList;
+        var input, search, ids, offset, limit, requestOptions, url, apiUrl, dataInfo, coinList;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
                     input = opts.input;
+                    search = input.search, ids = input.ids, offset = input.offset, limit = input.limit;
                     requestOptions = {
                         method: "GET",
                         redirect: "follow",
                     };
                     url = API + "/assets?";
-                    if (input.search !== null) {
-                        url += "search=".concat(input.search, "&");
+                    if (search !== null) {
+                        url += "search=".concat(search, "&");
                     }
-                    if (input.ids !== null) {
-                        url += "ids=".concat(input.ids.join(), "&");
+                    if (ids !== null) {
+                        url += "ids=".concat(ids.join(), "&");
                     }
-                    if (input.offset !== null && input.limit !== null) {
-                        url += "offset=".concat(input.offset, "&limit=").concat(input.limit, "&");
+                    if (offset !== null && limit !== null) {
+                        url += "offset=".concat(offset, "&limit=").concat(limit, "&");
                     }
                     ;
                     return [4 /*yield*/, fetch(url, requestOptions)];
@@ -116,5 +117,118 @@ var appRouter = trpc.router({
             }
         });
     }); }),
+    getCoinById: trpc.procedure
+        .input(function (id) {
+        if (typeof id === 'string') {
+            return id;
+        }
+        throw new Error('Error[getCoinById]: Input is not a string.');
+    })
+        .query(function (opts) { return __awaiter(void 0, void 0, void 0, function () {
+        var id, requestOptions, apiUrl, dataInfo, storageCoin, coin;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    id = opts.input;
+                    requestOptions = {
+                        method: "GET",
+                        redirect: "follow",
+                    };
+                    return [4 /*yield*/, fetch(API + "/assets/".concat(id), requestOptions)];
+                case 1:
+                    apiUrl = _a.sent();
+                    return [4 /*yield*/, apiUrl.json()];
+                case 2:
+                    dataInfo = _a.sent();
+                    storageCoin = dataInfo.data;
+                    coin = {
+                        id: storageCoin.id,
+                        rank: storageCoin.rank,
+                        symbol: storageCoin.symbol,
+                        name: storageCoin.name,
+                        supply: (0, helper_1.formatSupply)(storageCoin.supply, storageCoin.symbol),
+                        maxSupply: (0, helper_1.formatSupply)(storageCoin.maxSupply, storageCoin.symbol),
+                        marketCapUsd: (0, helper_1.formatMarketCap)(storageCoin.marketCapUsd),
+                        volumeUsd24Hr: (0, helper_1.formatVolumeUsd24Hr)(storageCoin.volumeUsd24Hr),
+                        priceUsd: (0, helper_1.formatPrice)(storageCoin.priceUsd),
+                        changePercent24Hr: (0, helper_1.formatPercent)(storageCoin.changePercent24Hr),
+                        vwap24Hr: storageCoin.vwap24Hr,
+                        logo: "images/coins/".concat(storageCoin.symbol.toLowerCase(), ".svg"),
+                    };
+                    return [2 /*return*/, coin];
+            }
+        });
+    }); }),
+    getCoinHistory: trpc.procedure
+        .input(function (value) {
+        var valueAsType = value;
+        var isValidObject = value !== null
+            && typeof value === 'object';
+        var isValidId = (value === null || value === void 0 ? void 0 : value.hasOwnProperty('id'))
+            && typeof valueAsType.id === 'string';
+        var isValidInterval = (value === null || value === void 0 ? void 0 : value.hasOwnProperty('interval'))
+            && typeof valueAsType.interval === 'string'
+            && valueAsType.interval === valueAsType.interval;
+        var isValidEnd = (value === null || value === void 0 ? void 0 : value.hasOwnProperty('end'))
+            && (valueAsType.end === null
+                || (typeof valueAsType.end === 'string'
+                    && new Date(valueAsType.end).toString() !== 'Invalid Date'));
+        var isValidStart = (value === null || value === void 0 ? void 0 : value.hasOwnProperty('start'))
+            && (valueAsType.start === null
+                || (typeof valueAsType.start === 'string'
+                    && new Date(valueAsType.start).toString() !== 'Invalid Date'));
+        if (isValidObject
+            && isValidId
+            && isValidInterval
+            && isValidEnd
+            && isValidStart) {
+            var endDate = valueAsType.end === null
+                ? null
+                : new Date(valueAsType.end);
+            var startDate = valueAsType.start === null
+                ? null
+                : new Date(valueAsType.start);
+            var validValue = {
+                id: valueAsType.id,
+                interval: valueAsType.interval,
+                end: endDate,
+                start: startDate,
+            };
+            return validValue;
+        }
+        throw new Error('Error[getCoinHistory]: Input is not a valid object.');
+    })
+        .query(function (opts) { return __awaiter(void 0, void 0, void 0, function () {
+        var input, id, interval, end, start, requestOptions, url, apiUrl, dataInfo, coinHistory;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    input = opts.input;
+                    id = input.id, interval = input.interval, end = input.end, start = input.start;
+                    requestOptions = {
+                        method: "GET",
+                        redirect: "follow",
+                    };
+                    url = (end !== null && start !== null)
+                        ? API + "/assets/".concat(id, "/history?interval=").concat(interval, "&end=").concat(+end, "&start=").concat(+start)
+                        : API + "/assets/".concat(id, "/history?interval=").concat(interval);
+                    return [4 /*yield*/, fetch(url, requestOptions)];
+                case 1:
+                    apiUrl = _a.sent();
+                    return [4 /*yield*/, apiUrl.json()];
+                case 2:
+                    dataInfo = _a.sent();
+                    coinHistory = dataInfo.data.map(function (data) {
+                        var newCoinHistory = {
+                            priceUsd: (0, helper_1.formatPrice)(data.priceUsd),
+                            time: data.time,
+                            date: new Date(data.time),
+                        };
+                        return newCoinHistory;
+                    });
+                    return [2 /*return*/, coinHistory];
+            }
+        });
+    }); })
 });
 exports.default = appRouter;

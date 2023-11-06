e792fb60504d21d0298c1a6f16766513d5ae0c54
darya-yarosh <gurimusagi@yandex.ru>
2023-11-06 17:11:25 +0300

    Removed CoinCap controller and loader.

--- client/src/logic/loaders/coinList.ts
+++
@@ -1,6 +0,0 @@
-import coinCapController from "logic/storage/CoinCapController";
-
-export async function coinListLoader() {
-  const coinList = await coinCapController.getCoinList();
-  return coinList;
-}

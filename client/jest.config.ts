4d615085d71cc82640bc82d3327e9ca11f01c642
darya-yarosh <gurimusagi@yandex.ru>
2023-11-08 23:38:35 +0300

    Installed Jest settings and dependencies.

---
+++ client/jest.config.ts
@@ -0,0 +1,18 @@
+import type {Config} from 'jest';
+import {defaults} from 'jest-config';
+
+const config: Config = {
+  preset: 'ts-jest',
+  verbose: true,
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  moduleDirectories: ["node_modules", "src"],
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
+  collectCoverage: true,
+  coverageReporters: ['json', 'html', 'text'],
+};
+
+export default config;

776f6f12fe5b5d7092b103e05a52d4d804249d07
darya-yarosh <gurimusagi@yandex.ru>
2023-11-20 13:22:19 +0300

    Updated error detection when loading coins with invalid id in cypress tests (navigation). Formatted navigation tests in cypress.

--- client/cypress/e2e/navigation.cy.js
+++ client/cypress/e2e/navigation.cy.js
@@ -10,51 +10,62 @@ describe('App navigation', () => {
     })
 
     it('Go to the Bitcoin cryptocurrency page', () => {
-        cy.contains('table > tbody tr td', /Bitcoin/i).click();
-        cy.url().then(($url) => {
-            if (!$url.includes(`/cryptocoins/bitcoin`)) {
-                throw new Error("Not a valid url")
-            }
-        })
+        cy.contains('table > tbody tr td', /Bitcoin/i)
+            .click();
+        cy.url()
+            .then(($url) => {
+                if (!$url.includes(`/cryptocoins/bitcoin`)) {
+                    throw new Error("Not a valid url")
+                }
+            })
     })
-    
+
     it('Go to the transaction form', () => {
         cy.visit(`/cryptocoins/bitcoin`)
-        cy.contains('button', 'Buy').click()
-        cy.url().then(($url) => {
-            if (!$url.includes(`/purchase/bitcoin`)) {
-                throw new Error("Not a valid url")
-            }
-        })
+        cy.contains('button', 'Buy')
+            .click()
+        cy.url()
+            .then(($url) => {
+                if (!$url.includes(`/purchase/bitcoin`)) {
+                    throw new Error("Not a valid url")
+                }
+            })
     })
 
     it('Go to the to the portfolio page', () => {
-        cy.contains('label', /My portfolio/i).click();
-        cy.url().then(($url) => {
-            if (!$url.includes(`/portfolio`)) {
-                throw new Error("Not a valid url")
-            }
-        })
+        cy.contains('label', /My portfolio/i)
+            .click();
+        cy.url()
+            .then(($url) => {
+                if (!$url.includes(`/portfolio`)) {
+                    throw new Error("Not a valid url")
+                }
+            })
     })
 
     it('Go to the to the portfolio page and return', () => {
-        cy.contains('label', /My portfolio/i).click();
-        cy.url().then(($url) => {
-            if (!$url.includes(`/portfolio`)) {
-                throw new Error("Not a valid url")
-            }
-        })
-        cy.get('*[class^="PortfolioCard_header"] button').click();
-        cy.url().then(($url) => {
-            if (!$url.includes(`/`)) {
-                throw new Error("Not a valid url")
-            }
-        })
+        cy.contains('label', /My portfolio/i)
+            .click();
+        cy.url()
+            .then(($url) => {
+                if (!$url.includes(`/portfolio`)) {
+                    throw new Error("Not a valid url")
+                }
+            })
+        cy.get('*[class^="PortfolioCard_header"] button')
+            .click();
+        cy.url()
+            .then(($url) => {
+                if (!$url.includes(`/`)) {
+                    throw new Error("Not a valid url")
+                }
+            })
     })
 
     it('Go to the page with the invalid name of the cryptocurrency and see the error page', () => {
         cy.on('uncaught:exception', (err, runnable) => {
-            if (err.message.includes('Invalid code id')) {
+            const msg = 'Error loading the specified coin from the database: invalid coin id.'
+            if (err.message.includes(msg)) {
                 console.log('Application Error')
                 return false;
             }
@@ -62,35 +73,45 @@ describe('App navigation', () => {
             return true
         })
         cy.visit(`/cryptocoins/bitcoinc`)
-        cy.wait(10000)
+            .wait(5000)
+        cy.url()
+            .then(($url) => {
+                if (!$url.includes(`/cryptocoins/bitcoinc`)) {
+                    throw new Error("Not a valid url")
+                }
+            })
         cy.contains('Something wrong')
     })
 
     it('Go to the previous page (main page)', () => {
-        cy.visit(`/cryptocoins/bitcoin`)
-        cy.get('*[class^="TextCard_header"] button:first').click();
-        cy.url().then(($url) => {
-            if (!$url.includes(`/`)) {
-                throw new Error("Not a valid url")
-            }
-        })
+        cy.visit(`/cryptocoins/bitcoin`).wait(500)
+        cy.get('*[class^="TextCard_header"] button:first')
+            .click();
+        cy.url()
+            .then(($url) => {
+                if (!$url.includes(`/`)) {
+                    throw new Error("Not a valid url")
+                }
+            })
     })
-    
+
     it('Go to the previous page (another page)', () => {
         cy.visit(`/cryptocoins/bitcoin`)
         cy.visit(`/purchase/bitcoin`)
         cy.go('back')
-        cy.url().then(($url) => {
-            if (!$url.includes(`/cryptocoins/bitcoin`)) {
-                throw new Error("Not a valid url")
-            }
-        })
+        cy.url()
+            .then(($url) => {
+                if (!$url.includes(`/cryptocoins/bitcoin`)) {
+                    throw new Error("Not a valid url")
+                }
+            })
 
         cy.go('back')
-        cy.url().then(($url) => {
-            if (!$url.includes(`/`)) {
-                throw new Error("Not a valid url")
-            }
-        })
+        cy.url()
+            .then(($url) => {
+                if (!$url.includes(`/`)) {
+                    throw new Error("Not a valid url")
+                }
+            })
     })
 })

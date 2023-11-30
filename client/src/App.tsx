35f4360dea35a5e3e1188c8e918abcb1d93700af
darya-yarosh <gurimusagi@yandex.ru>
2023-11-30 19:50:17 +0300

    Updated client's request to the server.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -64,7 +64,7 @@ export default function App() {
 		trpc.createClient({
 			links: [
 				httpBatchLink({
-					url: 'https://yds-cryptocurrency-server.netlify.app/.netlify/functions/trpc',
+					url: 'https://project-deployment--yds-cryptocurrency-server.netlify.app/trpc',
 				}),
 			],
 		})
